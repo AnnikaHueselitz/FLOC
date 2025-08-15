e25ffc0c60bf18431d3fbffbfaf8eca6e92887f7
annika <smiley.4ever.h@gmail.com>
2025-08-15 12:37:02 +0200

    Added function to calculate the threshold for FLOC running with multiple bin sizes simultaneously

--- R/threshold_tuning.R
+++ R/threshold_tuning.R
@@ -182,3 +182,228 @@ max_quantile <- function(q, r, eta,  max_Tstat, max_Tstat_sorted){
   return(detect < r * eta)
 
 }
+
+#'threshold tuning
+#'
+#'tunes the thrsehold for the FLOC algorithm to reach a desired false alarm
+#'probability
+#'
+#'@param NJ integer; number of data points in a jump bin
+#'@param NK integer; number of data points in a kink bin
+#'@param tau integer; number of observations so that P(hat tau < tau) <= eta
+#'@param k integer; number of observations that are available as historical data
+#'@param eta numerical between 0 ane 1, probability such that
+#'P(hat tau < tau) <= eta
+#'@param arl integer; desired average run length, tau has to be NULL to tune for
+#'the average run length
+#'@param r integer; number of simulated maximal test statistics
+#'@param data list with at least k + tau rows and r columns;
+#'data to tune the threshold on, if NULL standard normal data is generated
+#'
+#'@return list with:
+#'jump - threshold for only jump detector
+#'kink - threshold for only kink detector
+#'both_jump - threshold for jump part if running both detectors
+#'both_kink -threshold for kink part if running both detectors
+#'
+#'
+#'@export
+
+t_tuning_mult <- function(NJ, NK, k,tau = NULL, eta = NULL, arl = NULL, r = 1000,
+                     data = NULL){
+
+  #tune for the average run length in the case, that tau is set NULL
+  if(is.null(tau)){
+    tau <- arl
+    eta <- 1 - (1 / exp(1))
+  }
+
+  if(is.null(data)){
+    #generates r sets of data and calculates the maximal test statistics
+    max_Tstat <- replicate(r, max_Tstat_calc_mult(NJ, NK, tau, k), simplify = FALSE)
+  }
+  else{
+    r <- ncol(data)
+    max_Tstat <- sapply(1:r,function(i) max_Tstat_calc_mult(NJ, NK, tau,
+                                                       k, data[,i]), simplify = FALSE)
+  }
+
+
+  #sort the maximal test statistic
+  max_Tstat <- do.call(rbind,max_Tstat)
+  max_Tstat_sorted <- apply(max_Tstat, 2,sort)
+
+  #the sought quantile
+  q <- floor((1-eta) * r) + 1
+
+  #looking for the correct combined quantile
+  while(!max_quantile(q, r, eta, max_Tstat, max_Tstat_sorted)){
+    q <- q + 1
+  }
+
+  #setting the combined threshold as the quantile
+  rho <- max_Tstat_sorted[q,]
+
+  if(length(NJ) == 0){
+    rhoj <- c()
+    rhok <- rho
+  }
+  else if(length(NK) == 0){
+    rhoj <- rho
+    rhok <- c()
+  }
+  else{
+    rhoj <- rho[1:length(NJ)]
+    rhok <- rho[(length(NJ)+1):(length(NJ) + length(NK))]
+  }
+
+  thresholds = list(jump =  unname(rho[1:length(NJ)]), kink = unname(rhok))
+
+  return(thresholds)
+}
+
+#'maximal Test statistic calculate
+#'
+#'Internal function that generates data and calculates the maximal test
+#'statistic
+#'
+#'@param NJ integer; number of data points in a jump bin
+#'@param NK integer; number of data points in a kink bin
+#'@param tau integer; number of observations so that P(hat tau < tau) <= eta
+#'@param k integer; number of observations that are available as historical data
+#'@param datavec numerical vector; data to calculate the maximal Test statistic
+#'on
+#'
+#'@return numerical vector; the maximal jump test statistic and the maximal kink
+#'test statistic
+#'
+#'@importFrom stats rnorm
+#'@importFrom utils tail
+#'
+#'@keywords internal
+
+max_Tstat_calc_mult <- function(NJ, NK, tau, k, datavec = NULL){
+
+  jump <- length(NJ) > 0
+  kink <- length(NK) > 0
+
+  if(is.null(datavec)){
+    #generate historical data if no data is given
+    data <- rnorm(k)
+  }
+  else{
+    data <- head(datavec, k)
+  }
+
+  #estimate prechange distribution
+  x <- (1-k):0
+  f <- lm(data ~ x)
+
+
+  if(is.null(datavec)){
+    #generate data if no data vector is given
+    data <- c(tail(data, 3 * max(NJ, NK)), rnorm(tau))
+  }
+  else{
+    data <- c(tail(data, 3 * max(NJ, NK)), tail(datavec, -k ))
+  }
+
+  data <- data - unname(predict(f,list(x = (1 - 3 * max(NJ, NK)):tau)))
+
+  #initialize bins
+
+  if(jump){
+    SJ <- sapply(NJ,S_init, data = data[1:(3 * max(NJ, NK)) ])
+  }
+  if(kink){
+    SK <- sapply(NK,S_init, data = data[1:(3 * max(NJ, NK)) ])
+    W <- sapply(NK,W_init, data = data[1:(3 * max(NJ, NK)) ])
+  }
+
+  #initialize vector of test statistics
+  Tstat_jump <- c()
+  Tstat_kink <- c()
+
+  #iterates to calculate the test statistics
+  for(i in 1:tau){
+
+    if(jump){
+
+      #number of observations in last bin
+      rJ <- sapply(NJ, function(x) (i -1) %% x + 1)
+
+      #shift the bins if a new bin is started
+      for(j in 1:length(NJ)){
+
+        if(rJ[j] == 1){
+          SJ[,j] <- c(SJ[-1,j],0)
+        }
+
+        SJ[3,j] <- SJ[3,j] + data[i + 3 * NJ[j]]
+      }
+
+      #calculate new test statistics
+      Tstat_jump <- rbind(Tstat_jump,
+                          sapply(1:length(NJ),
+                                 function(i) Tstat_j_calc(S = SJ[,i], N = NJ[i],
+                                                          r = rJ[i])))
+
+    }
+    if(kink){
+      rK <- sapply(NK, function(x) (i -1) %% x + 1)
+
+
+      for(j in 1:length(NK)){
+
+        if(rK[j] == 1){
+          SK[,j] <- c(SK[-1,j],0)
+          W[,j] <- c(W[-1,j],0)
+        }
+
+        SK[3,j] <- SK[3,j] + data[i + 3 * NK[j]]
+        W[3,j] <- W[3,j] + rK[j] * data[i + 3 * NK[j]]
+      }
+
+      Tstat_kink <- rbind(Tstat_kink,
+                          sapply(1:length(NK),
+                            function(i) Tstat_k_calc(S = SK[,i], W = W[,i],
+                                                     N = NK[i], r = rK[i])))
+    }
+  }
+
+  #take maximal test statistic
+  if(jump){
+    Tstat_jump <-apply(abs(Tstat_jump), 2, max)
+  }
+  if(kink){
+    Tstat_kink <- apply(abs(Tstat_kink),2,max)
+  }
+
+  return(c(Tstat_jump,Tstat_kink))
+}
+
+#'maximal test statistic quantile
+#'
+#'Internal function that takes a quantile of the jump and kink and checks if the
+#'desired quantile for both test statistics combined is reached.
+#'
+#'@param q integer; quantile of the singular test statistics
+#'@param r integer; number of maximal test statistics
+#'@param eta numerical between 0 and 1; desired probability for a false alarm
+#'@param max_Tstat numercial list; maximal test statistics
+#'@param max_Tstat_sorted numerical list; maximal test statistics sorted
+#'
+#'@return boolean; true if the desired quantile is reached
+#'
+#'@keywords internal
+
+max_quantile <- function(q, r, eta,  max_Tstat, max_Tstat_sorted){
+
+  #counts test statistics that are greater than  the threshold at quantile q
+  detect <- apply(max_Tstat, 1, function(x) any(x >= max_Tstat_sorted[q,]))
+  detect <- sum(detect)
+
+  #returns true if the count is below the desired quantile
+  return(detect < r * eta)
+
+}
